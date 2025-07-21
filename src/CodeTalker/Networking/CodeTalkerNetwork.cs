33137fb9bec521b347fa46af10a37ea49f9f9441
Robyn <rnixon98@gmail.com>
2025-07-21 15:18:53 -0500

    feat: network drops packets the client cannot deserialize

--- src/CodeTalker/Networking/CodeTalkerNetwork.cs
+++ src/CodeTalker/Networking/CodeTalkerNetwork.cs
@@ -27,7 +27,7 @@ public static class CodeTalkerNetwork
   private const ushort NETWORK_PACKET_VERSION = 2;
 
   internal static string CODE_TALKER_SIGNATURE = $"!!CODE_TALKER_NETWORKING:PV{NETWORK_PACKET_VERSION}!!";
-  private static readonly Dictionary<Type, PacketListener> packetListeners = [];
+  private static readonly Dictionary<string, PacketListener> packetListeners = [];
 
   internal static string GetTypeNameString(Type type) =>
     $"{type.Assembly.GetName().Name},{type.DeclaringType?.Name ?? "NONE"}:{type.Namespace ?? "NONE"}.{type.Name}";
@@ -46,13 +46,14 @@ public static class CodeTalkerNetwork
   public static bool RegisterListener<T>(PacketListener listener) where T : PacketBase
   {
     var inType = typeof(T);
+    string typeName = GetTypeNameString(inType);
 
-    if (packetListeners.ContainsKey(inType))
+    if (packetListeners.ContainsKey(typeName))
     {
       return false;
     }
 
-    packetListeners.Add(inType, listener);
+    packetListeners.Add(typeName, listener);
 
     return true;
   }
@@ -63,8 +64,11 @@ public static class CodeTalkerNetwork
   /// <param name="packet">The packet to send, must be derived from PacketBase</param>
   public static void SendNetworkPacket(PacketBase packet)
   {
-    var rawPacket = $"{CODE_TALKER_SIGNATURE}{JsonConvert.SerializeObject(packet, PacketSerializer.JSONOptions)}";
-    var bytes = Encoding.UTF8.GetBytes(rawPacket);
+    string rawPacket = JsonConvert.SerializeObject(packet, PacketSerializer.JSONOptions);
+    PacketWrapper wrapper = new(GetTypeNameString(packet.GetType()), rawPacket);
+
+    var rawWrapper = $"{CODE_TALKER_SIGNATURE}{JsonConvert.SerializeObject(wrapper, PacketSerializer.JSONOptions)}";
+    var bytes = Encoding.UTF8.GetBytes(rawWrapper);
     SteamMatchmaking.SendLobbyChatMsg(new(SteamLobby._current._currentLobbyID), bytes, bytes.Length);
   }
 
@@ -91,7 +95,7 @@ public static class CodeTalkerNetwork
       CodeTalkerPlugin.Log.LogDebug($"Full message: {data}");
     }
 
-
+    PacketWrapper wrapper;
     PacketBase packet;
     Type inType;
 
@@ -100,7 +104,40 @@ public static class CodeTalkerNetwork
 
     try
     {
-      if (JsonConvert.DeserializeObject<PacketBase>(data, PacketSerializer.JSONOptions) is PacketBase inPacket)
+      if (JsonConvert.DeserializeObject<PacketWrapper>(data, PacketSerializer.JSONOptions) is PacketWrapper inWrapper)
+        wrapper = inWrapper;
+      else
+        throw new InvalidOperationException("Failed to deserialize a valid packet wrapper");
+    }
+    catch (Exception ex)
+    {
+      string aData;
+
+      if (data.Length < 24)
+        aData = data;
+      else
+        aData = data[..24];
+
+      CodeTalkerPlugin.Log.LogError($"""
+      Error while receiving a packet!
+      Exception: {ex.GetType().Name}
+      Abridged Packet:
+        {aData}
+      """);
+      return;
+    }
+
+    if (!packetListeners.TryGetValue(wrapper.PacketType, out var listener))
+    {
+      if (dbg)
+        CodeTalkerPlugin.Log.LogDebug($"Skipping packet of type: {wrapper.PacketType} because this client does not have it installed, this is safe!");
+
+      return;
+    }
+
+    try
+    {
+      if (JsonConvert.DeserializeObject<PacketBase>(wrapper.PacketPayload, PacketSerializer.JSONOptions) is PacketBase inPacket)
       {
         inType = inPacket.GetType();
         packet = inPacket;
@@ -108,70 +145,41 @@ public static class CodeTalkerNetwork
       else
         return;
     }
-    catch (JsonReaderException)
+    catch (Exception ex)
     {
-      CodeTalkerPlugin.Log.LogWarning($"""
-      Malformed JSON in packet!
-      Abridged Packet:
-        {data[18..108]}
-
+      CodeTalkerPlugin.Log.LogError($"""
+      Error while unwrapping a packet!
+      Exception: {ex.GetType().Name}
+      Expected Type: {wrapper.PacketType}
       """);
       return;
     }
-    catch (JsonSerializationException)
-    {
-      //Silently ignore this unless debugging
-      if (!dbg)
-        return;
 
-      CodeTalkerPlugin.Log.LogDebug($"""
-      Unable to serialize a packet!
-      This may be because our mods differ from the sender's!
-      Stack Trace: 
-      Abridged Packet:
-        {data[18..108]}
-      """);
-      return;
+    if (dbg)
+      CodeTalkerPlugin.Log.LogDebug($"Sending an event for type {wrapper.PacketType}");
+
+    try
+    {
+      listener.Invoke(new(senderID.m_SteamID), packet);
     }
     catch (Exception ex)
     {
-      CodeTalkerPlugin.Log.LogDebug($"""
-      Error while handling a packet!
-      Exception: {ex.GetType().Name}
-      Abridged Packet:
-        {data[18..108]}
+      var plugins = Chainloader.PluginInfos;
+      var mod = plugins.Values.Where(mod => mod.Instance?.GetType().Assembly == inType.Assembly).FirstOrDefault();
+
+      //Happy lil ternary
+      string modName = mod != null
+        ? $"{mod.Metadata.Name} version {mod.Metadata.Version}"
+        : inType.Assembly.GetName().Name;
+
+      //Big beefin' raw string literal with interpolation
+      CodeTalkerPlugin.Log.LogError($"""
+      The following mod encountered an error while responding to a network packet, please do not report this as a CodeTalker error!
+        Mod: {modName}
+        StackTrace:
+      {ex}
       """);
-      return;
     }
-
-    if (packetListeners.TryGetValue(inType, out var listener))
-    {
-      if (dbg)
-        CodeTalkerPlugin.Log.LogDebug($"Sending an event for type {inType.Name}");
-
-      try
-      {
-        listener.Invoke(new(senderID.m_SteamID), packet);
-      }
-      catch (Exception ex)
-      {
-        var plugins = Chainloader.PluginInfos;
-        var mod = plugins.Values.Where(mod => mod.Instance?.GetType().Assembly == inType.Assembly).FirstOrDefault();
-
-        //Happy lil ternary
-        string modName = mod != null
-          ? $"{mod.Metadata.Name} version {mod.Metadata.Version}"
-          : inType.Assembly.GetName().Name;
-
-        //Big beefin' raw string literal with interpolation
-        CodeTalkerPlugin.Log.LogError($"""
-        The following mod encountered an error while responding to a network packet, please do not report this as a CodeTalker error!
-          Mod: {modName}
-          StackTrace:
-        {ex}
-        """);
-      }
-    }
-
   }
+
 }
