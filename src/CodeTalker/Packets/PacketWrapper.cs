45a67b90bda605a7214481a2e65dce888ba23b51
Robyn <rnixon98@gmail.com>
2025-07-21 15:18:04 -0500

    feat: packet wrapper for packet metadata

---
+++ src/CodeTalker/Packets/PacketWrapper.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+
+namespace CodeTalker.Packets;
+
+internal class PacketWrapper
+{
+  [JsonProperty]
+  public readonly string PacketType;
+
+  [JsonProperty]
+  public readonly string PacketPayload;
+
+  [JsonConstructor]
+  public PacketWrapper(string PacketType, string PacketPayload)
+  {
+    this.PacketType = PacketType;
+    this.PacketPayload = PacketPayload;
+  }
+}
